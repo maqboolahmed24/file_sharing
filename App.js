2572c0450220a2c65e0d4e8ff91d79a94bd49fe6
maqboolahmed <parziwal24@gmail.com>
2024-02-26 20:15:11 +0000

    almost finished

--- App.js
+++ App.js
@@ -6,6 +6,47 @@ function App() {
     const [filename, setFilename] = useState('');
     const [uploadStatus, setUploadStatus] = useState('');
     const [downloadStatus, setDownloadStatus] = useState('');
+    const [serverFileName, setServerFileName] = useState('');
+    //const [isAdminMode, setIsAdminMode] = useState(false);
+    const [showAdmin, setShowAdmin] = useState(false);
+    const [userName, setUserName] = useState('');
+
+    const [uploadsList, setUploadsList] = useState([]);
+
+    const adminKey = 'salah';
+
+    const enterAdminMode = () => {
+        const key = prompt('Please enter the admin key:');
+        if (key === adminKey) {
+            setShowAdmin(true); // This will be used to conditionally render the admin UI
+            // Clear other state variables if necessary
+
+            fetchUploads();  //calling feth function when wnetring admin mode
+        } else {
+            alert('Incorrect key!');
+        }
+    };
+
+
+
+    const fetchUploads = async () =>
+    {
+        try {
+            const response = await fetch('/uploads');
+            if (!response.ok) {
+                throw new Error('FIle Uploads.json fetch fail, please check the file present in the src folder');
+
+            }
+            const data = await response.json();
+            setUploadsList(data.uploads);
+        }
+        catch (error) {
+            console.error('error fetching uploas:',error)
+        }
+    }
+
+
+
 
     const handleFileChange = (event) => {
         setFile(event.target.files[0]);
@@ -15,23 +56,75 @@ function App() {
         setFilename(event.target.value);
     };
 
+    const copyToClipboard = (text) => {
+        // Newer clipboard API method
+        if (navigator.clipboard && window.isSecureContext) {
+            return navigator.clipboard.writeText(text);
+        } else {
+            // Fallback method
+            let textArea = document.createElement("textarea");
+            textArea.value = text;
+            textArea.style.position = "fixed"; // avoid scrolling to bottom
+            document.body.appendChild(textArea);
+            textArea.focus();
+            textArea.select();
+
+            try {
+                document.execCommand('copy');
+                alert('Text copied to clipboard');
+            } catch (err) {
+                console.error('Unable to copy to clipboard', err);
+            }
+
+            document.body.removeChild(textArea);
+        }
+    };
+
+
+
+
+
+
+
+    const deleteUpload = async (fileName) => {
+        try {
+            const response = await fetch(`/uploads/${fileName}`, { method: 'DELETE' });
+            if (!response.ok) {
+                throw new Error('Failed to delete');
+            }
+
+            //Filter out the deleted file from the uploadsList state
+            setUploadsList(uploadsList.filter((file) => file.fileName !== fileName));
+        } catch (error) {
+            console.error('Error deleting file:', error);
+        }
+    };
+
+
+
+
+
+
     const handleUpload = async () => {
         if (!file) {
-            alert('Please select a file first.');
+            alert('You must enter your name and select file :)');//exception when you click upload button without performing the necessory steps
             return;
         }
 
         const formData = new FormData();
         formData.append('file', file);
+        formData.append('userName', userName);
 
         try {
-            const response = await fetch('/upload', { // This line is making the fetch request
+            const response = await fetch('/upload', {
                 method: 'POST',
                 body: formData,
             });
 
+            const result = await response.json();
             if (response.ok) {
-                setUploadStatus('File uploaded successfully.');
+                setUploadStatus('File uploaded successfully. Below is your key to download');
+                setServerFileName(result.fileName); // Set the new filename here
             } else {
                 setUploadStatus('Upload failed.');
             }
@@ -43,7 +136,7 @@ function App() {
 
     const handleDownload = async () => {
         if (!filename) {
-            alert('Please enter a filename to download.');
+            alert('Please enter key to download.');
             return;
         }
 
@@ -73,25 +166,86 @@ function App() {
 
     return (
         <div className="App">
-            <div>
-                <h2>File Upload</h2>
-                <input type="file" onChange={handleFileChange} />
-                <button onClick={handleUpload}>Upload</button>
-                {uploadStatus && <p>{uploadStatus}</p>}
-            </div>
-            <div>
-                <h2>File Download</h2>
-                <input
-                    type="text"
-                    value={filename}
-                    onChange={handleFilenameChange}
-                    placeholder="Enter filename to download"
-                />
-                <button onClick={handleDownload}>Download</button>
-                {downloadStatus && <p>{downloadStatus}</p>}
-            </div>
+            {!showAdmin && (
+                <>
+                    <div>
+                        <h2>File Upload</h2>
+                        <input
+                            type="text"
+                            value={userName}
+                            onChange={(e) => setUserName(e.target.value)}
+                            placeholder="enter your name please"
+                            required
+                        />
+
+                        <input type="file" onChange={handleFileChange} />
+
+
+                        <button onClick={handleUpload}>Upload</button>
+                        {uploadStatus && <p>{uploadStatus}</p>}
+                        {serverFileName && (
+                            <div>
+                                <input value={serverFileName} readOnly />
+                                <button onClick={() => copyToClipboard(serverFileName)}>
+                                    Copy Key to Share
+                                </button>
+                            </div>
+                        )}
+                    </div>
+                    <div>
+                        <h2>File Download</h2>
+                        <input
+                            type="text"
+                            value={filename}
+                            onChange={handleFilenameChange}
+                            placeholder="Enter Key to download"
+                        />
+                        <button onClick={handleDownload}>Download</button>
+                        {downloadStatus && <p>{downloadStatus}</p>}
+                    </div>
+                    <button onClick={enterAdminMode} className="admin-button">Admin</button>
+                </>
+            )}
+
+
+
+
+
+
+            {showAdmin && (
+                <div>
+                    <h2>Hey admin, control the system</h2>
+                    <table>
+                        <thead>
+                            <tr>
+                                <th>Username</th>
+                                <th>File Name</th>
+                                <th>Upload Time</th>
+                                <th>Actions</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {uploadsList.map((upload, index) => (
+                                <tr key={index}>
+                                    <td>{upload.userName || 'Anonymous'}</td>
+                                    <td>{upload.fileName}</td>
+                                    <td>{new Date(upload.uploadTime).toLocaleString()}</td>
+                                    <td>
+                                        {/* Add button for delete action */}
+                                        <button onClick={() => deleteUpload(upload.fileName)}>Delete</button>
+                                    </td>
+                                </tr>
+                            ))}
+                        </tbody>
+                    </table>
+                </div>
+            )}
+
+
+
         </div>
     );
+
 }
 
-export default App;
+export default App; 
--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const fs = require('fs');
 const path = require('path');
 const cors = require('cors');
 const app = express();
+
 app.use(cors());
 
 
@@ -13,13 +14,13 @@ fs.existsSync(uploadsDir) || fs.mkdirSync(uploadsDir);
 
 // Multer setup for file uploads
 const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, uploadsDir)
-  },
-  filename: function (req, file, cb) {
-    const fileExt = path.extname(file.originalname);
-    cb(null, file.fieldname + '-' + Date.now() + fileExt);
-  }
+    destination: function (req, file, cb) {
+        cb(null, uploadsDir)
+    },
+    filename: function (req, file, cb) {
+        const fileExt = path.extname(file.originalname);
+        cb(null, file.fieldname + '-' + Date.now() + fileExt);
+    }
 });
 const upload = multer({ storage: storage });
 
@@ -28,29 +29,92 @@ app.use(express.static('public'));
 
 // Routes
 app.post('/upload', upload.single('file'), (req, res) => {
-  if (!req.file) {
-    return res.status(400).send('No file uploaded.');
-  }
-  res.send('File uploaded successfully.');
+    const userName = req.body.userName; //get the suername fromt the request loby
+    if (!req.file) {
+        return res.status(400).send('No file uploaded.');
+    }
+    //res.send('File uploaded successfully.');
+    res.json({ fileName: req.file.filename });//used to send the dilename to the front wnd, the server side file name not the origninal file name that is used as the key to doenload the file
+
+
+    // After the file is saved
+    const uploadsData = require('./uploads.json');
+    uploadsData.uploads.push({
+        userName: userName,
+        fileName: req.file.filename,
+        uploadTime: new Date().toISOString()
+    });
+    fs.writeFileSync('./uploads.json', JSON.stringify(uploadsData, null, 2));
+
+
 });
+
+
+
+
+
+app.delete('/uploads/:filename', (req, res) => {
+    const { filename } = req.params;
+    const filePath = path.join(uploadsDir, filename);
+
+
+
+    fs.unlink(filePath, (err) => {
+        if (err) {
+            console.error('error deleting the file : $(filename)', err);
+            return res.status(500).send('error deleting file..');
+        }
+        //upate uploads.json
+        const uploadsData = require('./uploads.json');
+        uploadsData.uploads = uploadsData.uploads.filter((file) => file.fileName != filename);
+        fs.writeFileSync('/uploads.json', JSON.stringify(uploadsData, null, 2));
+
+
+        res.send('File deleted successfullyyy.');
+    }
+    );
+}
+
+
+);
+
+
+
+
+
+
+
 
 app.get('/download/:filename', (req, res) => {
-  const filename = req.params.filename;
-  const filePath = path.join(uploadsDir, filename);
+    const filename = req.params.filename;
+    const filePath = path.join(uploadsDir, filename);
 
-  res.download(filePath, filename, (err) => {
-    if (err) {
-      if (err.code === 'ENOENT') {
-        console.error(`File not found: ${filename}`);
-        return res.status(404).send('File not found.');
-      }
-      console.error(`Error downloading file: ${filename}`, err);
-      return res.status(500).send('Error downloading file.');
-    }
-  });
+    res.download(filePath, filename, (err) => {
+        if (err) {
+            if (err.code === 'ENOENT') {
+                console.error(`File not found: ${filename}`);
+                return res.status(404).send('File not found.');
+            }
+            console.error(`Error downloading file: ${filename}`, err);
+            return res.status(500).send('Error downloading file.');
+        }
+    });
 });
+
+app.get('/admin', (req, res) => {
+    res.sendFile(path.join(__dirname, 'path-to-admin.html'));
+});
+
+
+
+
+app.get('/uploads', (req, res) => {
+    const uploadsData = require('./uploads.json');
+    res.json(uploadsData);
+});
+
 const port = 6000;
 // Start the server
-app.listen(port, '0.0.0.0', () => {
-  console.log(`Server listening at http://localhost:${port}`);
+app.listen(port, () => {
+    console.log(`Server listening at http://localhost:${port}`);
 });
